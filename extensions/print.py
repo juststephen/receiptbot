d4c2aeccd32dc9533b80d9d21bc171f428723aef
juststephen <62077126+juststephen@users.noreply.github.com>
2026-01-31 16:16:57 +0100

    feat: add print job queue
    Added a print job queue for when the printer is offline or when multiple jobs are issued at the same time.

--- extensions/print.py
+++ extensions/print.py
@@ -1,12 +1,11 @@
 import asyncio
+from collections.abc import Callable
 from discord import Attachment, app_commands, Interaction
 from discord.app_commands import AppCommandError
 from discord.ext import commands, tasks
 from escpos.exceptions import DeviceNotFoundError  # pyright: ignore[reportMissingTypeStubs]
 from escpos.printer import Network  # pyright: ignore[reportMissingTypeStubs]
 import logging
-import platform
-import subprocess
 from zoneinfo import ZoneInfo
 
 from characters import UnicodeCharacterPrinting
@@ -28,6 +27,11 @@ class Print(commands.Cog):
     """
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+
+        # Printing queue
+        self.queue: asyncio.Queue[Callable[[], None]] = asyncio.Queue(10)
+        self.printer_online_event: asyncio.Event = asyncio.Event()
+
         self.receipt_ongoing: bool = False
 
         self.printer_ip = '192.168.0.3'
@@ -36,30 +40,58 @@ class Print(commands.Cog):
 
         self.tz = ZoneInfo('Europe/Amsterdam')
 
-        self.task_ping_printer.start()
+        self.print_queue_worker.start()
+        self.task_printer_status.start()
+
+    async def check_printer_status(self) -> None:
+        """
+        Printer status check, clears character cache when down.
+        """
+        def is_online() -> bool:
+            """
+            Check if the printer is online.
+            """
+            self.printer.open()
+            printer_online = self.printer.is_online()
+            self.printer.close()
+            return printer_online
+
+        try:
+            printer_online = await asyncio.to_thread(is_online)
+        except:
+            printer_online = False
+
+        if printer_online != self.printer_online_event.is_set():
+            if printer_online:
+                logger.info('Printer is up')
+                self.printer_online_event.set()
+            else:
+                logger.info('Printer is down')
+                self.printer_online_event.clear()
+                # Clear unicode character cache
+                self.unicode_char_print.clear()
+
+    @tasks.loop(seconds=1)
+    async def print_queue_worker(self) -> None:
+        """
+        Print queue worker to perform blocking jobs in a thread.
+        """
+        while True:
+            job = await self.queue.get()
+            # Check if the printer is online
+            await self.check_printer_status()
+            # Wait if the printer is offline
+            await self.printer_online_event.wait()
+            # Perform print job
+            await asyncio.to_thread(job)
+            self.queue.task_done()
 
     @tasks.loop(seconds=30)
-    async def task_ping_printer(self) -> None:
+    async def task_printer_status(self) -> None:
         """
-        Printer ping task, clears character cache when down.
+        Printer status task.
         """
-        count_param = '-n' if platform.system().lower() == 'windows' else '-c'
-        command = ['ping', count_param, '1', '-w', '1', self.printer_ip]
-
-        proc = await asyncio.create_subprocess_exec(
-            *command,
-            stdin=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL
-        )
-        await proc.communicate()
-
-        if proc.returncode != 0:
-            # Clear unicode character cache
-            self.unicode_char_print.clear()
-            logging.info('Printer is down')
-        else:
-            logging.info('Printer is up')
+        await self.check_printer_status()
 
     @app_commands.command()
     @app_commands.checks.cooldown(1, 10)
@@ -82,7 +114,31 @@ class Print(commands.Cog):
         qr_content
             QR code content
         """
-        logging.info(f'Printing command issued by {interaction.user.name}')
+        def print_job() -> None:
+            """
+            Print job.
+            """
+            self.printer.open()
+
+            self.printer.set_with_default(bold=True, underline=True)
+            self.unicode_char_print.text(interaction.user.name)
+            self.printer.set(bold=False)
+            self.unicode_char_print.text(f' {created_at:%Y-%m-%d %H:%M}\n')
+            self.printer.set(
+                underline=False,
+                double_width=True, double_height=True
+            )
+
+            if message:
+                self.unicode_char_print.text(f'{message.strip()}\n')
+            if image:
+                print_image(self.printer, image_bytes)
+            if qr_content:
+                self.printer.qr(qr_content, size=8, center=True)  # pyright: ignore[reportUnknownMemberType]
+
+            self.printer.close()
+
+        logger.info(f'Printing command issued by {interaction.user.name}')
 
         if message is None and image is None and qr_content is None:
             await interaction.response.send_message(
@@ -105,29 +161,28 @@ class Print(commands.Cog):
 
         created_at = interaction.created_at.astimezone(self.tz)
 
-        self.printer.open()
-
-        self.printer.set_with_default(bold=True, underline=True)
-        self.unicode_char_print.text(interaction.user.name)
-        self.printer.set(bold=False)
-        self.unicode_char_print.text(f' {created_at:%Y-%m-%d %H:%M}\n')
-        self.printer.set(
-            underline=False,
-            double_width=True, double_height=True
-        )
-
-        if message:
-            self.unicode_char_print.text(f'{message.strip()}\n')
+        # Get the image data
         if image:
-            print_image(self.printer, await image.read())
-        if qr_content:
-            self.printer.qr(qr_content, size=8, center=True)  # pyright: ignore[reportUnknownMemberType]
+            image_bytes = await image.read()
 
-        self.printer.close()
+        # Put the job in the queue
+        try:
+            self.queue.put_nowait(print_job)
+        except asyncio.QueueFull:
+            await interaction.followup.send(
+                'Print queue is full, please try again later.'
+            )
+            return
 
         self.receipt_ongoing = True
 
-        await interaction.followup.send('Your message has been printed.')
+        if self.printer_online_event.is_set():
+            await interaction.followup.send('Your message has been printed.')
+        else:
+            await interaction.followup.send(
+                'Your message has been queued, '
+                'waiting for the printer to come back online.'
+            )
 
     @app_commands.command()
     @app_commands.checks.cooldown(1, 10)
@@ -135,20 +190,53 @@ class Print(commands.Cog):
         """
         Cut the receipt.
         """
-        logging.info(f'Cut command issued by {interaction.user.name}')
-
-        if self.receipt_ongoing:
-            await interaction.response.defer(thinking=True)
-
+        def cut_job() -> None:
+            """
+            Cut job.
+            """
             self.printer.open()
             self.printer._raw(feed_cut)  # pyright: ignore[reportPrivateUsage]
             self.printer.close()
 
+        logger.info(f'Cut command issued by {interaction.user.name}')
+
+        if self.receipt_ongoing:
+            await interaction.response.defer(thinking=True)
+
+            # Put the job in the queue
+            try:
+                self.queue.put_nowait(cut_job)
+            except asyncio.QueueFull:
+                await interaction.followup.send(
+                    'Print queue is full, please try again later.'
+                )
+                return
+
             self.receipt_ongoing = False
 
-            await interaction.followup.send('The receipt has been cut.')
+            if self.printer_online_event.is_set():
+                await interaction.followup.send('The receipt has been cut.')
+            else:
+                await interaction.followup.send(
+                    'Receipt cutting queued, '
+                    'waiting for the printer to come back online.'
+                )
         else:
             await interaction.response.send_message('No receipt to cut.')
+
+    @print_queue_worker.error
+    async def task_error(self, error: BaseException) -> None:
+        """
+        Method that handles erroneous tasks.
+        """
+        if isinstance(error, DeviceNotFoundError):
+            if self.printer_online_event.is_set():
+                logger.info('Printer is down')
+                self.printer_online_event.clear()
+                # Clear unicode character cache
+                self.unicode_char_print.clear()
+            return
+        logger.error(error)
 
     @print.error
     @cut.error
@@ -168,19 +256,15 @@ class Print(commands.Cog):
             )
             return
 
-        if (isinstance(error, app_commands.errors.CommandInvokeError)
-                and isinstance(error.original, DeviceNotFoundError)):
-            await interaction.followup.send(
-                'The printer is currently offline.'
-            )
-            # Clear unicode character cache
-            self.unicode_char_print.clear()
-            return
-
         logger.error(error)
-        await interaction.followup.send(
-            'Something went wrong, please try again later.'
-        )
+        if interaction.response.is_done():
+            await interaction.followup.send(
+                'Something went wrong, please try again later.'
+            )
+        else:
+            await interaction.response.send_message(
+                'Something went wrong, please try again later.'
+            )
 
 
 async def setup(bot: commands.Bot) -> None:
